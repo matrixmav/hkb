a694afcdaad199ed5f2b2677f4e95faa9db17464
nidhi <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-14 10:49:22 +0530

    updated

--- protected/views/user/login.php
+++ protected/views/user/login.php
@@ -1,10 +1,10 @@
 
-<div class="main">
+<!--<div class="main">
   <div class="container">
     <!-- BEGIN SIDEBAR & CONTENT -->
-    <div class="row margin-bottom-40">
+    <!--<div class="row margin-bottom-40">
       <!-- BEGIN CONTENT -->
-      <div class="col-md-9 col-sm-9">
+                <div class="col-md-6 col-sm-6">
         <?php 
         if(!empty($msg)){
             echo $msg; 
@@ -12,19 +12,19 @@
         <h1>Login</h1>
         <div class="content-form-page">
           <div class="row">
-            <div class="col-md-7 col-sm-7">
+                            <div class="col-md-12 col-sm-12">
                 <form class="form-horizontal form-without-legend" method="post" name="LoginForm" id=LoginForm" role="form" onsubmit="return validateFrm()">
                 <div class="form-group">
                   <label for="email" class="col-lg-4 control-label">User Name <span class="require">*</span></label>
                   <div class="col-lg-8">
-                      <input type="text" class="form-control" id="name" name="name">
+                      <input type="text" class="form-control" id="login-name" name="name">
                   </div>.
                   <span id="name_error"></span>
                 </div>
                 <div class="form-group">
                   <label for="password" class="col-lg-4 control-label">Password <span class="require">*</span></label>
                   <div class="col-lg-8">
-                      <input type="password" class="form-control" id="password" name="password">
+                      <input type="password" class="form-control" id="login-password" name="password">
                   </div>
                   <span id="password_error"></span>
                 </div>
@@ -68,23 +68,23 @@
         </div>
       </div>
       <!-- END CONTENT -->
-    </div>
+    <!--</div>
     <!-- END SIDEBAR & CONTENT -->
-  </div>
-</div>
+  <!--</div>
+</div>-->
 <script>
  function validateFrm() {
     $("#name_error").html("");
-    if ($("#name").val() == "") {
+    if ($("#login-name").val() == "") {
       $("#name_error").html("Enter User Name");
-      $("#name").focus();            
+      $("#login-name").focus();            
       return false;
     }
        
     $("#password_error").html("");
-    if ($("#password").val() == "") {
+    if ($("#login-password").val() == "") {
         $("#password_error").html("Enter Password");
-        $("#password").focus();
+        $("#login-password").focus();
         return false;
     } 
     
