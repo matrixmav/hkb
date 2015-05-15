0105683cc88097cb6a7af2c205a389cc057861ed
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-15 14:51:53 +0530

    updated

--- protected/views/layouts/footer.php
+++ protected/views/layouts/footer.php
@@ -5,15 +5,15 @@
         <!-- BEGIN BOTTOM ABOUT BLOCK -->
         <div class="col-md-2 col-sm-6 pre-footer-col">
           <p class="f-font">Legal</p>
-          <p class="f-cnt">legal@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:legal@xyz.com">legal@xyz.com</a><br/> 
           91-8500425185</p>
           
           <p class="f-font">Billing</p>
-          <p class="f-cnt">Billing@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:billing@xyz.com">Billing@xyz.com</a><br/> 
           91-8500425185</p>
           
           <p class="f-font">Support</p>
-          <p class="f-cnt">support@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:support@xyz.com">support@xyz.com</a><br/> 
           91-8500425185</p>
         </div>
         <!-- END BOTTOM ABOUT BLOCK -->
@@ -21,15 +21,15 @@
         <div class="col-md-2 col-sm-6 pre-footer-col">
        
           <p class="f-font">Member Enquiry</p>
-          <p class="f-cnt">memberenquiry@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:memberenquiry@xyz.com">memberenquiry@xyz.com</a><br/> 
           91-8500425185</p>
           
           <p class="f-font">Sales</p>
-          <p class="f-cnt">sales@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:sales@xyz.com">sales@xyz.com</a><br/> 
           91-8500425185</p>
           
           <p class="f-font">Info</p>
-          <p class="f-cnt">Info@xyz.com<br/> 
+          <p class="f-cnt"><a href="mailto:info@xyz.com">Info@xyz.com</a><br/> 
          91-8500425185</p>   
         </div>
          <div class="col-md-4 col-sm-6 pre-footer-col">
