cf1643e05a380db00e72fda0c6badb0c5dd16446
nidhi.sati <nidhi.sati@MGIS-0006.MGIS.COM>
2015-05-18 09:55:24 +0530

    updated

--- builder/USERSADMIN/menus/top_right.php
+++ builder/USERSADMIN/menus/top_right.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 function BlogUrl_BA($user){global $USE_ABSOLUTE_URLS,$BLOG_URL_FORMAT,$BLOG_DOMAIN;if(!$USE_ABSOLUTE_URLS){return "../site.php?user=".strtolower($user);}else if($BLOG_URL_FORMAT == 1){return "http://".strtolower($user).".".$BLOG_DOMAIN;}else if($BLOG_URL_FORMAT == 2){return "http://"."www.".$BLOG_DOMAIN."/".strtolower($user);}else{return "[blog format not defined]";}}
 
 $HTML='
